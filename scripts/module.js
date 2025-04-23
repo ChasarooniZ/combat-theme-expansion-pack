04b647e7807d264e9b8f70d0e85e4ed4884cae46
cadowtin <chasdowtin@gmail.com>
2025-04-23 10:22:48 -0500

    fixed sound filepaths

--- scripts/module.js
+++ scripts/module.js
@@ -3,9 +3,9 @@ Hooks.once("init", async function () {
   for (const theme of COMBAT_THEMES) {
     CONFIG.Combat.sounds[theme.id] = {
       label: `${MODULE_ID}.themes.${theme.id}`,
-      startEncounter: theme.startEncounter,
-      nextUp: theme.nextUp,
-      yourTurn: theme.yourTurn,
+      startEncounter: getSoundPaths(theme.startEncounter, theme.id),
+      nextUp: getSoundPaths(theme.nextUp, theme.id),
+      yourTurn: getSoundPaths(theme.yourTurn, theme.id),
     };
   }
 });
